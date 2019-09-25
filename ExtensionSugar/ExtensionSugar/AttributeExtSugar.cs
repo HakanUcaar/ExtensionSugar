3a409a18b4bd1f92b52eb24d545899b0f0dad283
Hakan Uçar <hakanucaar@yandex.com>
2019-09-25 12:14:40 +0300

    String Type Generic Sugarlar Eklendi

---
+++ ExtensionSugar/ExtensionSugar/AttributeExtSugar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionSugar
+{
+    public static class AttributeExtSugar
+    {
+        public static TValue GetAttributeValue<TAttribute, TValue>(
+            this Type type,
+            Func<TAttribute, TValue> valueSelector)
+            where TAttribute : Attribute
+        {
+            var att = type.GetCustomAttributes(
+                typeof(TAttribute), true
+            ).FirstOrDefault() as TAttribute;
+            if (att != null)
+            {
+                return valueSelector(att);
+            }
+            return default(TValue);
+        }
+
+        public static TValue GetPropAttributeValue<TAttribute, TValue>(
+                            this Type type,
+                            string MemberName,
+                            Func<TAttribute, TValue> valueSelector,
+                            bool inherit = false)
+                        where TAttribute : Attribute
+        {
+            var att = type
+                        .GetMember(MemberName)
+                            .FirstOrDefault()
+                            .GetCustomAttributes(
+                                                typeof(TAttribute),
+                                                inherit
+                                                )
+                                                .FirstOrDefault() as TAttribute;
+            if (att != null)
+            {
+                return valueSelector(att);
+            }
+            return default(TValue);
+        }
+    }
+}
