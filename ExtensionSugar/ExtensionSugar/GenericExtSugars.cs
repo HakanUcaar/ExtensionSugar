03f5688c627183f77a470ef435aa0b2bcd2b3df9
Hakan Uçar <hakanucaar@yandex.com>
2020-04-26 23:07:22 +0300

    TimeZone LocalDate, Base64Decode , Base64Encode

--- ExtensionSugar/ExtensionSugar/GenericExtSugars.cs
+++ ExtensionSugar/ExtensionSugar/GenericExtSugars.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,21 @@ namespace ExtensionSugar
     {
         public static bool IsNull<T>(this T obj) 
         {
+            if (obj is string)
+            {
+                return (obj as string).IsNullOrEmpty();
+            }
             return obj == null;
         }
         public static bool IsNotNull<T>(this T obj) 
         {
+            if (obj is string)
+            {
+                return !(obj as string).IsNullOrEmpty();
+            }
             return obj != null;
         }
+
         public static T With<T>(this T item, Action<T> action)
         {
             if (item.IsNotNull())
@@ -23,6 +33,14 @@ namespace ExtensionSugar
                 action(item);
             }
             return item;
+        }
+        public static TInput Do<TInput>(this TInput o, Action<TInput> action)
+        {
+            if (o.IsNotNull())
+            {
+                action(o);
+            }
+            return o;
         }
 
         public static bool In<T>(this T obj, params T[] args)
@@ -54,6 +72,40 @@ namespace ExtensionSugar
             {
                 return failureValue;
             }
+        }
+        public static T To<T>(this object o) where T : class
+        {
+            try
+            {
+                return o as T;
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    return (T)Convert.ChangeType(o, typeof(T));
+                }
+                catch (Exception)
+                {
+                    return default(T);
+                }                
+            }            
+        }
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+            return enumeration;
+        }
+
+        public static object GetPropertyValue<T>(this T classInstance, string propertyName)
+        {
+            PropertyInfo property = classInstance.GetType().GetProperty(propertyName);
+            if (property != null)
+                return property.GetValue(classInstance, null);
+            return null;
         }
     }
 }
--- ExtensionSugar/ExtensionSugar/StringExtSugars.cs
+++ ExtensionSugar/ExtensionSugar/StringExtSugars.cs
@@ -546,5 +546,15 @@ namespace ExtensionSugar
         {
             return str.Split(separator, StringSplitOptions.None).Select(s => (T)Convert.ChangeType(s, typeof(T)));
         }
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
     }
 }
